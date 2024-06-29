fa04d1ad3df2d0d8388aa8a5f2aa3f4d5abc20f9
paramesh-75 <parameshwaranit22@gmail.com>
2024-06-29 14:50:20 +0530

    create post added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -31,7 +31,7 @@ const Post = ({ post }) => {
 		},
 		onSuccess: (data) => {
 			toast.success("Post deleted successfully")
-			queryClient.invalidateQueries({queryKey: ["posts"]})
+			queryClient.invalidateQueries({queryKey: ["posts5"]})
 		}
 	})
 	const postOwner = post.user;
